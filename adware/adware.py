732f6f32616a38452b7e59422afc194346631c7a
AndranikAdyan <andranikadyan@gmail.com>
2025-01-25 15:38:31 +0400

    add adware

---
+++ adware/adware.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import random
+from PIL import Image
+import webbrowser
+import time
+
+adds_path = "adds"
+
+imgs = os.listdir(adds_path)
+
+def open_image():
+	img = Image.open(os.path.join(adds_path, random.choice(imgs)))
+	img.show()
+
+links = ["instagram.com", "youtube.com", "facebook.com"]
+def open_link():
+	link = random.choice(links)
+	webbrowser.open(link)
+
+def main():
+	func = random.choice([open_image, open_link])
+	func()
+
+while True:
+	time.sleep(3)
+	main()
